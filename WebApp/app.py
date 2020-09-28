95949ad2d189dc1d6ceca50cfa0e50255fb0e249
KuznetsovaViktoria <kuznetzowa.viktoria@gmail.com>
2020-09-28 10:44:34 +0300

    add WebApp

---
+++ WebApp/app.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    Date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/create-article', methods = ['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        return render_template("create_article.html")
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.Date.desc()).all()
+    return render_template("posts.html", articles=articles)
+
+if __name__ == "__main__":
+    app.run(debug=True)
